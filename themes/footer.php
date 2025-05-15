f7ccd7bba1c6272e4bca5d749792e9c90ed1646f
sydney <sydney0902@icloud.com>
2025-05-15 14:16:37 +0200

    les4 bootstrap

--- themes/footer.php
+++ themes/footer.php
@@ -1,7 +1,15 @@
-<footer>
-    <p>&copy; <?php echo date('Y'); ?> Surinaamse Recepten. Alle rechten voorbehouden.</p>
-  </footer>
+<!-- Footer -->
+<footer class="footer mt-auto py-3 bg-body-tertiary">
+  <div class="container">
+    <span class="text-body-secondary">Place footer content here.</span>
+  </div>
+</footer>
 
-  <?php wp_footer(); ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+<?php wp_footer(); ?>
 </body>
 </html>
--- themes/index.php
+++ themes/index.php
@@ -1,9 +1,15 @@
-
 <?php get_header(); ?>
 
-<main>
-  <h1>Surinaamse Recepten</h1>
-  <!-- Receptenlijst/filter hier -->
+<main class="container my-5">
+  <h1 class="mt-5">Dit is mijn eerste titel</h1>
+  <div class="col-lg-8 px-0">
+    <p class="fs-5">
+      Intro
+    </p>
+    <p>
+      Content
+    </p>
+  </div>
 </main>
 
 <?php get_footer(); ?>
